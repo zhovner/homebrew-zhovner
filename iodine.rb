b14388d0ea8ab8e9ef2c8541457b10d841c46245
Pavel Zhovner <pavel@zhovner.com>
2016-09-17 03:14:46 +0300

    iodine move to head from release

--- iodine.rb
+++ iodine.rb
@@ -1,8 +1,7 @@
 class Iodine < Formula
   desc "Tunnel IPv4 data through a DNS server"
   homepage "http://code.kryo.se/iodine/"
-  url "http://code.kryo.se/iodine/iodine-0.7.0.tar.gz"
-  version "0.7.0"
+  url "https://github.com/yarrick/iodine.git"
   sha256 "ad2b40acf1421316ec15800dcde0f587ab31d7d6f891fa8b9967c4ded93c013e"
 
   def install
