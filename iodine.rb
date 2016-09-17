a95ee0dff234c315eb4262b337d517bdd3475b52
Pavel Zhovner <pavel@zhovner.com>
2016-09-17 03:19:11 +0300

    iodine move to head from release

--- iodine.rb
+++ iodine.rb
@@ -1,8 +1,8 @@
 class Iodine < Formula
   desc "Tunnel IPv4 data through a DNS server"
   homepage "http://code.kryo.se/iodine/"
-  url "https://github.com/yarrick/iodine.git"
-  sha256 "ad2b40acf1421316ec15800dcde0f587ab31d7d6f891fa8b9967c4ded93c013e"
+  url "https://github.com/yarrick/iodine/archive/8e15a73a77c9973270d8aaf86a1cb683d28fbea5.zip"
+  sha256 "357920f31de9eb3971fb846f8da961997951560b5c38fe9e07e02714f876badb"
 
   def install
     system "make", "install", "prefix=#{prefix}"
