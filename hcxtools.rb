2e5fea207949a3b5b60b2ad5e2789f4f846cd1cf
Pavel Zhovner <pavel@zhovner.com>
2020-04-23 00:13:17 +0300

    Update hcxtools.rb

--- hcxtools.rb
+++ hcxtools.rb
@@ -16,7 +16,7 @@ class Hcxtools < Formula
     ENV.prepend "CPPFLAGS", "-I#{Formula["openssl@1.1"].opt_include}"
     ENV.prepend "LDFLAGS", "-L#{Formula["openssl@1.1"].opt_lib}"
 
-    bin.install
+    system "make", "install", "prefix=#{prefix}"
   end
 
   test do
