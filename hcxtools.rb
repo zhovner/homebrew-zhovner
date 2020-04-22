eafa98b69282c1adf39cbe0a6c90665f0869616e
Pavel Zhovner <pavel@zhovner.com>
2020-04-22 23:08:09 +0300

    Update hcxtools.rb
    Release hcxtools

--- hcxtools.rb
+++ hcxtools.rb
@@ -1,7 +1,9 @@
 class Hcxtools < Formula
   desc "Portable solution for capturing wlan traffic and conversion to hashcat formats (recommended by hashcat) and to John the Ripper formats. hcx: h = hash, c = capture, convert and calculate candidates, x = different hashtypes"
-  homepage "https://github.com/ZerBea/hcxtools"
-  head "https://github.com/ZerBea/hcxtools.git"
+  homepage "https://github.com/ZerBea/hcxtools" 
+  url "https://github.com/ZerBea/hcxtools/archive/6.0.2.tar.gz"
+  sha256 "963029bf00d3a98f2caa934d5765a91e29f5d1b0e0036a4c53328b5cf9c3c003"
+  
 
   head do
     url "https://github.com/ZerBea/hcxtools.git"
